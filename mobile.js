0bcb686d37ad85a16fd693dcb91d120d7a28b707
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2024-01-25 23:21:11 +0300

    06

--- mobile.js
+++ mobile.js
@@ -3,14 +3,14 @@ if (
     navigator.userAgent,
   )
 ) {
-  console.log('Вы используете мобильное устройство (телефон или планшет).');
+  // console.log('Вы используете мобильное устройство (телефон или планшет).');
   document.querySelector('.pc').classList.add('hidden');
   document.querySelector('.line').classList.add('hidden');
   document.querySelector('.frameFooter').classList.add('hidden');
   document.querySelector('.mySwiper ').classList.add('hidden');
   document.querySelector('.mobile').classList.remove('hidden');
 } else {
-  console.log('Вы используете ПК.');
+  // console.log('Вы используете ПК.');
   document.querySelector('.mobile').classList.add('hidden');
   document.querySelector('.mySwiper3').classList.add('hidden');
   document.querySelector('.knopki').classList.add('hidden');
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
+////////////////////////// Swiper1
 const swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
   loop: true,
@@ -19,18 +20,15 @@ const swiper = new Swiper('.mySwiper', {
 });
 const next = document.querySelector('.carousel-button');
 next.addEventListener('click', () => {
-  console.log('вперед');
   swiper.slideNext();
 });
 
 const prev = document.querySelector('.carousel-button-left');
 prev.addEventListener('click', () => {
-  console.log('назад');
   swiper.slidePrev();
 });
 
 ////////////////////////// Swiper2
-const numberOfSlides = document.querySelectorAll('.mySwiper2').length;
 var swiper2 = new Swiper('.mySwiper2', {
   cssMode: true,
   navigation: {
@@ -45,7 +43,7 @@ var swiper2 = new Swiper('.mySwiper2', {
   on: {
     init: function () {
       if (this.activeIndex === 0) {
-        console.log('первый');
+       
         document.querySelector('.btn2').classList.toggle('opac');
       } else {
         document.querySelector('.btn2').classList.toggle('opac');
@@ -55,21 +53,18 @@ var swiper2 = new Swiper('.mySwiper2', {
 });
 swiper2.on('slideChange', function () {
   if (swiper2.activeIndex === 0) {
-    console.log('первый');
     document.querySelector('.btn2').classList.add('opac');
   } else {
     document.querySelector('.btn2').classList.remove('opac');
   }
 });
 const next2 = document.querySelector('.btn1');
-next2.addEventListener('click', () => {
-  console.log('вперед');
+next2.addEventListener('click', () => {;
   swiper2.slideNext();
 });
 
 const prev2 = document.querySelector('.btn2');
 prev2.addEventListener('click', () => {
-  console.log('назад');
   swiper2.slidePrev();
 });
 
@@ -94,12 +89,10 @@ const swiper3 = new Swiper('.mySwiper3', {
 
 const next3 = document.querySelector('.buton-go');
 next3.addEventListener('click', () => {
-  console.log('вперед');
   swiper3.slideNext();
 });
 
 const prev3 = document.querySelector('.buton-nazad');
 prev3.addEventListener('click', () => {
-  console.log('назад');
   swiper3.slidePrev();
 });
